ae3602b20604384c0c09f7e95b30392118dae098
Michael Phelps <mpphelps@gmail.com>
2024-01-16 17:57:19 -0500

    Create generic data fetching hook

---
+++ src/hooks/useGenres.ts
@@ -0,0 +1,13 @@
+import useData from "./useData";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+
+const useGenres = () => {
+  return useData<Genre>('/genres')
+};
+
+export default useGenres;
