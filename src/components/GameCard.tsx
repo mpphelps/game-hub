81a499908db2515fb11a3f91c117d2a3cac74442
Michael Phelps <mpphelps@gmail.com>
2024-01-17 11:43:02 -0500

    fixed geturl bug if url is null

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,9 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
+      {game.background_image ? (
+        <Image src={getCroppedImageUrl(game.background_image)} />
+      ) : null}
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justifyContent={"space-between"}>
