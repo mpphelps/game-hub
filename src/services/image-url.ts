22d98e01b14a41cd0ab205b4feab47f4abf3e802
Michael Phelps <mpphelps@gmail.com>
2024-01-17 13:06:33 -0500

    Fix the issue with Chakra Menus

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -2,7 +2,6 @@ import noImage from '../assets/no-image-placeholder-6f3882e0.webp'
 
 const getCroppedImageUrl = (url:string)=>{
     if (!url) return noImage;
-    console.log("getting cropped image for: " + url);
     const target = 'media/';
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/'+url.slice(index);
