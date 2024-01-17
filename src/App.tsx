0f8ef1e71fc4f192325b22c0e7f19424f343d70a
Michael Phelps <mpphelps@gmail.com>
2024-01-17 11:19:34 -0500

    highlight selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={setselectedGenre} />
+          <GenreList
+            onSelectGenre={setselectedGenre}
+            selectedGenre={selectedGenre}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
